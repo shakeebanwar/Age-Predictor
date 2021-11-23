7d12843af994555ef88ab315fb64d3979f44856d
muhammadharis1 <mh7365496@gmail.com>
2021-11-23 19:15:42 +0500

    done all changes

--- gad.py
+++ gad.py
@@ -2,6 +2,8 @@ import cv2
 import math
 import argparse
 
+# python gad.py --image faiz.jpeg
+
 def highlightFace(net, frame, conf_threshold=0.7):
     frameOpencvDnn=frame.copy()
     frameHeight=frameOpencvDnn.shape[0]
